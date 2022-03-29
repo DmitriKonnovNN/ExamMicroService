c3c2bf595ea621f9b7622118f0ba1559b501f20f
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-03-29 18:11:34 +0400

    roll-back

--- src/main/java/io/dmitrikonnov/demo/ExamType.java
+++ src/main/java/io/dmitrikonnov/demo/ExamType.java
@@ -4,9 +4,12 @@ package io.dmitrikonnov.demo;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table (name = "EXAM_TYPE")
+@Table (name = "EXAM_TYPE", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "ID", name = "exam_type_id_unique_constraint"),
+        @UniqueConstraint(columnNames = "TYPE_NAME", name = "exam_type_name_unique_constraint")})
 @Data
 @NoArgsConstructor
 public class ExamType {
@@ -14,10 +17,16 @@ public class ExamType {
     @Id
     @SequenceGenerator(name = "examtype_sequence",
             sequenceName = "examtype_sequence",
-            allocationSize = 1)
+            allocationSize = 1,
+            initialValue = 10)
     @GeneratedValue(strategy = GenerationType.SEQUENCE,
             generator = "examtype_sequence")
+
+    @Column (nullable = false, name = "ID")
     Long id;
+
+    @Column(nullable = false, name = "TYPE_NAME")
+    @NotBlank
     String typeName;
     String description;
 
