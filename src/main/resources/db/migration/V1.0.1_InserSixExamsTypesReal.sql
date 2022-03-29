bbd168aca904215aa21ca33f4e9111a33069182f
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-03-30 00:15:06 +0400

    some initial insertions into db

---
+++ src/main/resources/db/migration/V1.0.1_InserSixExamsTypesReal.sql
@@ -0,0 +1,6 @@
+insert into exam_type(id,type_name,description) values (1,'Wirtschaft','') on conflict do nothing;
+insert into exam_type(id,type_name,description) values (2,'Jugendliche','Prüfung für Jugendliche unter 16') on conflict do nothing;
+insert into exam_type(id,type_name,description) values (3,'Erwachsene','Prüfung für Erwachsene ab 16') on conflict do nothing;
+insert into exam_type(id,type_name,description) values (4,'Migration','') on conflict do nothing;
+insert into exam_type(id,type_name,description) values (5,'Geflüchtete','') on conflict do nothing;
+insert into exam_type(id,type_name,description) values (6,'Medizin','') on conflict do nothing;
