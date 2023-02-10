1aac71da0bcba3d10d86ccfe5d64ff98830efba5
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2023-02-10 20:46:28 +0400

    terraform added

---
+++ terraform/data.tf
@@ -0,0 +1,9 @@
+data "aws_iam_role" "ebs-service-role" {
+  name = "aws-elasticbeanstalk-service-role"
+}
+
+data "aws_elastic_beanstalk_solution_stack" "docker-stack" {
+  most_recent = true
+  name_regex  = "^64bit Amazon Linux (.*) running Docker(.*)$"
+}
+#64bit Amazon Linux 2 v3.5.4 running Docker
